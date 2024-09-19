438311d4af3112aa5797ebeb53abe6369dffd66c
TranGiaKhanh2712 <s4041377@rmit.edu.vn>
2024-09-19 22:41:40 +0700

    Create rating.cpp

---
+++ rating.cpp
@@ -0,0 +1,39 @@
+#include "Rating_System.h"
+#include <iostream>
+
+// Constructor to initialize rating system
+Rating::Rating(const std::string& name, int rating, const std::string& review)
+    : name(name), rating(rating), review(review) {}
+
+// Getters
+std::string Rating::getMemberName() const {
+    return name;
+}
+
+int Rating::getRating() const {
+    return rating;
+}
+
+std::string Rating::getReview() const {
+    return review;
+}
+
+// Setters
+void Rating::setMemberName(const std::string& personName) {
+    this->name = personName;
+}
+
+void Rating::setRating(int rating) {
+    this->rating = rating;
+}
+
+void Rating::setReview(const std::string& review) {
+    this->review = review;
+}
+
+// Function to display rating details
+void Rating::displayRating() const {
+    std::cout << "Person Name: " << name << "\n";
+    std::cout << "Rating: " << rating << " stars\n";
+    std::cout << "Review: " << review << "\n";
+}
